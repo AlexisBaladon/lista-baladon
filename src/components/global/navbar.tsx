5cd9562387063d1143257d80c3ac7b494a0d325b
alexis.baladon <alexis.baladon@fing.edu.uy>
2023-01-14 17:14:39 -0300

    bottom navbar

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,17 +1,51 @@
 import React from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, View, Image } from 'react-native'
 
 const Navbar = () => {
   return (
-    <Text>
-        Buenas
-    </Text>
+    <View style={styles.navbar}>
+      <Image 
+          style={[styles.icon, styles.homeIcon].flat()}
+          source={require('./home.png')}
+      />
+      <Image 
+          style={[styles.icon, styles.cartIcon].flat()}
+          source={require('./cart.png')}
+      />
+      <Image 
+          style={[styles.icon, styles.likedIcon].flat()}
+          source={require('./liked.png')}
+      />
+    </View>
   )
 }
 
+const iconWidth = 11;
+
 const styles = StyleSheet.create({
     navbar: {
-        width: '100%',
+        width: '100%', height: 90,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0,
+        borderTopRightRadius: 50, borderTopLeftRadius: 50,
+        backgroundColor: 'white',
+    },
+    icon: {
+      marginTop: 5,
+      width: `${iconWidth}%`, height: `${iconWidth*3.5}%`,
+    },
+    homeIcon: {
+      marginLeft: 42.5,
+    },
+    cartIcon: {
+      width: `${iconWidth*1.75}%`, height: `${iconWidth*6}%`,
+    },
+    likedIcon: {
+      marginRight: 42.5,
     }
 })
 
